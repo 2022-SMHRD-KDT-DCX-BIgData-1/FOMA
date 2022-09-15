9f2937b27ca77027fd8d39ce38a7a6990c763538
user <user@DESKTOP-RF3H2VG>
2022-09-15 15:59:56 +0900

    20220915 게시판 기본 작성중

---
+++ foma_maven/src/main/java/com/saeyan/dao/BoardDAO.java
@@ -0,0 +1,191 @@
+package com.saeyan.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+
+
+import com.foma_java_mvc_folder.database.SqlSessionManager;
+import com.saeyan.dto.BoardVO;
+
+public class BoardDAO {
+	
+	
+	private BoardDAO() {
+	}
+
+	private static BoardDAO instance = new BoardDAO();
+
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	
+
+	   SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	   SqlSession sqlSession = sqlSessionFactory.openSession();
+	   
+	   
+
+	public List<BoardVO> selectAllBoards() {  //게시판 모든 글 검색
+		//String sql = "select * from board order by num desc";
+		List<BoardVO> list = new ArrayList<BoardVO>();
+
+		  try {//검색결과를 리스트로 받아온다
+			  list = sqlSession.selectList("selectAllBoards");
+		         if (list != null) {
+		            sqlSession.commit();
+
+		         } else {
+		            sqlSession.rollback();
+		         }
+		      } finally {
+		         sqlSession.close();
+		      }
+		      return list;
+		   }
+
+	public void insertBoard(BoardVO bVo) { // 게시판 업데이트 리턴값 int
+//		String sql = "insert into board("
+//				+ "num, name, email, pass, title, content) "
+//				+ "values(board_seq.nextval, ?, ?, ?, ?, ?)";
+		
+		int cnt = 0;
+		try {
+			// Mapper-insert 태그의 id, 매개변수
+			cnt = sqlSession.insert("insertBoard", bVo);
+
+			if (cnt > 0) {
+				sqlSession.commit();
+			} else {
+				sqlSession.rollback();
+			}
+
+		} finally {
+			sqlSession.close();
+		}
+		//return cnt;
+	}
+/*
+	public void updateReadCount(String num) {
+		String sql = "update board set readcount=readcount+1 where num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+
+	// 게시판 글 상세 내용 보기 :글번호로 찾아온다. : 실패 null,
+	public BoardVO selectOneBoardByNum(String num) {
+		String sql = "select * from board where num = ?";
+		BoardVO bVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+				bVo.setReadcount(rs.getInt("readcount"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		return bVo;
+	}
+
+	public void updateBoard(BoardVO bVo) {
+		String sql = "update board set name=?, email=?, pass=?, "
+				+ "title=?, content=? where num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bVo.getName());
+			pstmt.setString(2, bVo.getEmail());
+			pstmt.setString(3, bVo.getPass());
+			pstmt.setString(4, bVo.getTitle());
+			pstmt.setString(5, bVo.getContent());
+			pstmt.setInt(6, bVo.getNum());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+
+	public BoardVO checkPassWord(String pass, String num) {
+		String sql = "select * from board where pass=? and num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO bVo = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pass);
+			pstmt.setString(2, num);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setReadcount(rs.getInt("readcount"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return bVo;
+	}
+
+	public void deleteBoard(String num) {
+		String sql = "delete board where num=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	*/
+}
