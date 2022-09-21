bf1971143e13e1dfc1db43b63f569dbaa722c7bf
user <user@DESKTOP-RF3H2VG>
2022-09-21 17:51:12 +0900

    findfriends.jsp 수정!(con,dao)

--- foma_maven/src/main/java/com/foma_java_mvc_folder/Controller/findIdCon.java
+++ foma_maven/src/main/java/com/foma_java_mvc_folder/Controller/findIdCon.java
@@ -22,7 +22,12 @@ public class findIdCon extends HttpServlet {
 		HttpSession session = request.getSession();
 		//String findIdMember = (Member)session.getAttribute("findIdMember");
 		String username = request.getParameter("find");
+		System.out.println("파라미터 이름 "+username);
 
+		if(username==null||username.equals("")||username.isBlank()||username.isEmpty()) {
+			username="null";
+		}
+			
 		//2. 수집된 데이터를 Member 객체에 담기
 
 		//Member vo = new Member(username);
@@ -47,6 +52,7 @@ public class findIdCon extends HttpServlet {
 				System.out.println(m.getUsername());
 			}
 			System.out.println("친구 찾기 성공!");
+			System.out.println("친구 검색결과 : "+findId.size());// 검색결과 몇개인지 확인
 			//아이디 체크 확인하면 친구찾기 성공
 			session.setAttribute("findIdMember", findId);
 			response.sendRedirect("./template/test1_foodblog/FindFriends.jsp");
